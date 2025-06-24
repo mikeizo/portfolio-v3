7deed6dc4af85048e41ea54adf79d1308aa64ae7
Mike Troepa <mtropea55@gmail.com>
2025-06-23 22:55:55 -0400

    First commit!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import { loadEnv } from 'vite'
+import vue from '@astrojs/vue'
+
+const env = loadEnv(process.env, process.cwd(), '')
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: parseInt(env.PORT)
+  },
+  integrations: [vue()]
+
+})
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <div class="logo"></div>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Work</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
