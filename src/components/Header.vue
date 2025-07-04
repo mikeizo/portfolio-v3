e13534ec90a70c003509f80e360aa3ae79444b86
Mike Troepa <mtropea55@gmail.com>
2025-07-03 23:03:47 -0400

    Add Styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <div>Header</div>
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>Work</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+  <header class="header">
+    <div class="header-content">
+      <div class="logo">
+        <h1>Portfolio</h1>
+      </div>
+      <nav class="nav">
+        <ul class="nav-list">
+          <li><a href="/" class="nav-link">Home</a></li>
+          <li><a href="/work" class="nav-link">Work</a></li>
+          <li><a href="/about" class="nav-link">About</a></li>
+          <li><a href="/contact" class="nav-link">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
