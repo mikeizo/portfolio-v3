5fe668ae8436664238c0f588af3898b6a1a08fa2
Mike Troepa <mtropea55@gmail.com>
2025-07-24 00:30:48 -0400

    Update nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,57 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref, onMounted } from 'vue'
+  import logoUrl from '@/assets/images/logo.svg'
+
+  const isOpen = ref(false)
+
+  const openNav = () => {
+    isOpen.value = !isOpen.value
+  }
+
+  const showMenu = () => {
+    if (window.innerWidth >= 768) {
+      isOpen.value = true
+    } else {
+      isOpen.value = false
+    }
+  }
+
+  onMounted(() => {
+    showMenu()
+    // Add a resize event listener to update isOpen based on window width
+    window.addEventListener('resize', () => showMenu())
+  })
+</script>
 
 <template>
   <header class="header">
-    <div class="header-content">
-      <div class="logo">
-        <h1>Portfolio</h1>
-      </div>
-      <nav class="nav">
-        <ul class="nav-list">
-          <li><a href="/" class="nav-link">Home</a></li>
-          <li><a href="/work" class="nav-link">Work</a></li>
-          <li><a href="/about" class="nav-link">About</a></li>
-          <li><a href="/contact" class="nav-link">Contact</a></li>
+    <div class="header__logo">
+      <img :src="logoUrl.src" alt="Mike Tropea Logo" height="32" width="150" />
+    </div>
+    <div v-show="isOpen" class="header__nav">
+      <nav class="header__nav-menu">
+        <ul class="nav__menu">
+          <li class="nav__link nav__link--active">
+            <a href="/">Home</a>
+          </li>
+          <li class="nav__link">
+            <a href="/work">Work</a>
+          </li>
+          <li class="nav__link">
+            <a href="/about">About</a>
+          </li>
         </ul>
       </nav>
+      <button class="nav__contact">Contact</button>
     </div>
+    <button
+      class="header__burger"
+      aria-label="Toggle mobile menu"
+      @click="openNav"
+    >
+      <span />
+      <span />
+      <span />
+    </button>
   </header>
 </template>
