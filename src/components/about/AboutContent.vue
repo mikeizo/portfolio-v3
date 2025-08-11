4a2f085b8a19c434be8a5acf8fa919ec3424b83e
Mike Troepa <mtropea55@gmail.com>
2025-08-10 23:16:23 -0400

    Add pages and update styles

---
+++ src/components/about/AboutContent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+  const props = defineProps<{
+    data: string
+  }>()
+
+  console.log('test props', props)
+
+  defineOptions({ name: 'AboutContent' })
+</script>
+
+<template>
+  <div class="about__content">
+    <div v-html="data" />
+    <img
+      class="about_image"
+      src="/public/images/mike-tropea.jpg"
+      alt="Mike Tropea"
+      width="316"
+      height="395"
+    />
+  </div>
+</template>
---
+++ src/components/about/AboutList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import type { AboutType } from '@/types/portfolio'
+
+  const props = defineProps<{
+    data: AboutType
+  }>()
+
+  defineOptions({ name: 'AboutContent' })
+</script>
+
+<template>
+  <div class="about__list">
+    <div v-for="(item, index) in data" class="about__list-item">
+      <div class="about__list-year">
+        <span>{{ item.yearFrom }}</span>
+        <span>{{ item.yearTo }}</span>
+      </div>
+      <div class="about_list-description">
+        {{ item.description }}
+      </div>
+    </div>
+  </div>
+</template>
