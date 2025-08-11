f223f7449cbf619b33ffb817e8e85b308f3fb3aa
Mike Troepa <mtropea55@gmail.com>
2025-08-10 23:14:19 -0400

    Add types

---
+++ src/types/portfolio.d.ts
@@ -0,0 +1,16 @@
+export type MenuType = {
+  title: string
+  url: string
+}[]
+
+export type SettingsType = {
+  about: string
+  email: string
+}[]
+
+export type AboutType = {
+  yearFrom: string
+  yearTo?: string
+  description: string
+  image: string
+}[]
