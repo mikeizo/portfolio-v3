bb407df074f332418ee10546fb81710e12a430a7
Mike Troepa <mtropea55@gmail.com>
2025-10-14 23:28:50 -0400

    Add Icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  defineProps<{
+    name: string
+    height?: number
+    width?: number
+    alt?: string
+  }>()
+
+  defineOptions({ name: 'Icon' })
+</script>
+
+<template>
+  <svg
+    :height="height"
+    :width="width"
+    :class="`icon icon--${name}`"
+    :aria-label="name"
+  >
+    <use xlink:href="#close" />
+  </svg>
+</template>
