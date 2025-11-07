730b8813ac77be59231d33bc24d3eac297180904
Mike Troepa <mtropea55@gmail.com>
2025-11-06 19:27:16 -0500

    Add theme toggle and restructure nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-  import { ref, onMounted } from 'vue'
-  import menuItems from '@/assets/api/menu.json'
+  import { ref, computed, onMounted } from 'vue'
   import Logo from '@/components/Logo.vue'
+  import Nav from '@/components/Nav.vue'
+  import Icon from '@/components/Icon.vue'
 
-  const props = defineProps<{
+  defineProps<{
     path: string
   }>()
 
@@ -14,48 +15,68 @@
     isOpen.value = !isOpen.value
   }
 
-  const isActive = (url: string) => {
-    return props.path === url
+  const toggleTheme = () => {
+    document.documentElement.setAttribute('data-theme', theme.value)
+    localStorage.setItem('theme', theme.value)
+
+    toggleMode.value = !toggleMode.value
   }
 
+  const toggleMode = ref(true)
+  const theme = computed(() => (toggleMode.value ? 'light' : 'dark'))
+
   onMounted(() => {
-    // TODO: Add intersection observer
+    const savedValue = localStorage.getItem('theme')
+
+    if (savedValue === 'light') {
+      toggleTheme()
+    }
   })
 
   defineOptions({ name: 'Header' })
 </script>
 
 <template>
+  <div v-show="isOpen" class="nav__mobile-backdrop" @click="openNav"></div>
   <header class="header wrapper" :class="{ sticky: isSticky }">
     <div class="header__content">
-      <a class="header__logo" href="/" aria-label="Home Page">
-        <Logo />
-      </a>
-      <div :class="{ 'header__nav--is-open': isOpen }" class="header__nav">
-        <nav class="header__nav-menu">
-          <ul class="nav__menu">
-            <li
-              v-for="(item, index) in menuItems"
-              :key="item.title"
-              class="nav__link"
-              :class="{ 'nav__link--active': isActive(item.url) }"
-              :aria-label="item.title"
-            >
-              <a :href="item.url">{{ item.title }}</a>
-            </li>
-          </ul>
-        </nav>
+      <div class="header__logo">
+        <a class="" href="/" aria-label="Home Page">
+          <Logo />
+        </a>
+      </div>
+
+      <Nav :path="path" class="nav__desktop" />
+
+      <div class="header__desktop">
+        <button class="header__theme">
+          <Icon :name="theme" :height="20" :width="20" @click="toggleTheme" />
+        </button>
         <button class="nav__contact" aria-label="Contact">Contact</button>
       </div>
-      <button
-        class="header__burger"
-        aria-label="Toggle mobile menu"
-        @click="openNav"
-      >
-        <span />
-        <span />
-        <span />
-      </button>
+
+      <div class="header__mobile">
+        <div
+          :class="{ 'header__mobile-nav--is-open': isOpen }"
+          class="header__mobile-nav"
+        >
+          <Nav :path="path" class="nav__mobile" />
+          <button class="nav__contact" aria-label="Contact">Contact</button>
+        </div>
+
+        <button class="header__theme">
+          <Icon :name="theme" :height="20" :width="20" @click="toggleTheme" />
+        </button>
+        <button
+          class="header__burger"
+          aria-label="Toggle mobile menu"
+          @click="openNav"
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+      </div>
     </div>
   </header>
 </template>
---
+++ src/components/Nav.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  import menuItems from '@/assets/api/menu.json'
+
+  const props = defineProps<{
+    path: string
+  }>()
+
+  const isActive = (url: string) => {
+    return props.path === url
+  }
+
+  defineOptions({ name: 'Nav' })
+</script>
+
+<template>
+  <nav class="nav">
+    <ul class="nav__menu">
+      <li
+        v-for="(item, index) in menuItems"
+        :key="item.title"
+        class="nav__link"
+        :class="{ 'nav__link--active': isActive(item.url) }"
+        :aria-label="item.title"
+      >
+        <a :href="item.url">{{ item.title }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
